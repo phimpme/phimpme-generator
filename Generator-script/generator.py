2588e64024a0b50dea587069d64e2b0e3d375c34
Yu Zhiqiang <rp@yuzhiqiang.org>
2014-06-09 14:17:19 +0800

    Enable / disable functions

--- Generator-script/generator.py
+++ Generator-script/generator.py
@@ -4,28 +4,17 @@
 # To test it, simplely run this file
 # The output APK will be put in the working directory
 
-import os
+import os, re
 
 def set_name(path, app_name):
-    raw_str = '<string name="app_name">Phimpme</string>'
+    re_str = r'<string name="app_name">.*</string>'
     target_str = '<string name="app_name">' + app_name + '</string>'
     strings_xml = path + "/Phimpme/src/main/res/values/strings.xml"
-
-    # Read strings.xml
-    file = open(strings_xml, 'r')
-    lines = file.readlines()
-    file.close()
     
-    # Raplace app_name
-    for i in range(0, len(lines)):
-        lines[i] = lines[i].replace(raw_str, target_str)
-        if lines[i].find(target_str) != -1:
-            break
-    
-    # Write back
-    file = open(strings_xml, 'w')
-    file.writelines(lines)
-    file.close()
+    content = open(strings_xml, 'r').read()
+    content, count = re.subn(re_str, target_str, content)
+    assert(count == 1)
+    open(strings_xml, 'w').write(content)
 
 def set_package_name(path, package_name):
     # manifest = path + "/Phimpme/src/main/AndroidManifest.xml"
@@ -35,7 +24,17 @@ def set_logo(path, app_logo):
     pass
 
 def set_config(path, configs):
-    pass
+    configuration_java = path + "/Phimpme/src/main/java/com/phimpme/phimpme/Configuration.java"
+    content = open(configuration_java, 'r').read()
+    for (k,v) in configs.items():
+        # RegEx string, something like "ENABLE_XXX = true;"
+        # "\s*" means 0 or more spaces
+        # TODO: make sure k contains only capitals and underlines
+        re_str = k + r"\s*=.*;"
+        target_str = k + " = " + ('true' if v else 'false') + ";"
+        content, count = re.subn(re_str, target_str, content)
+        assert(count == 1)
+    open(configuration_java, 'w').write(content)
 
 def compile(path):
     gradlew = path + "/gradlew"
@@ -62,4 +61,4 @@ if __name__ == "__main__":
     generate(template_path="/Users/yzq/Documents/GitHub/The-Generator/Android/Phimpme",
         app_name="YZQ-Phimpme",
         app_logo=None,
-        configs=None)
+        configs={'ENABLE_MAP': False, 'ENABLE_PHOTO_MANIPULATION': False})
