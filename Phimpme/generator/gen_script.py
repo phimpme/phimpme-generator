d6668bd99c0554bd837c1ba1435f37d6cbfd91c8
Yu Zhiqiang <rp@yuzhiqiang.org>
2014-06-14 00:53:54 +0800

    Django website and generator script work together

--- Phimpme/generator/gen_script.py
+++ Phimpme/generator/gen_script.py
@@ -11,7 +11,7 @@ def set_name(path, app_name):
     target_str = '<string name="app_name">' + app_name + '</string>'
     strings_xml = path + "/Phimpme/src/main/res/values/strings.xml"
     
-    content = open(strings_xml, 'r').read()
+    content = open(strings_xml, 'r').read().encode('utf-8')
     content, count = re.subn(re_str, target_str, content)
     assert(count == 1)
     open(strings_xml, 'w').write(content)
@@ -23,15 +23,17 @@ def set_package_name(path, package_name):
 def set_logo(path, app_logo):
     pass
 
-def set_config(path, configs):
+def set_enable(path, enables):
     configuration_java = path + "/Phimpme/src/main/java/com/phimpme/phimpme/Configuration.java"
     content = open(configuration_java, 'r').read()
-    for (k,v) in configs.items():
+    # Disable all before enabling selected features
+    content = re.sub(r'(ENABLE_.*=).*;', r'\1 false;', content)
+    for feature in enables:
         # RegEx string, something like "ENABLE_XXX = true;"
         # "\s*" means 0 or more spaces
-        # TODO: make sure k contains only capitals and underlines
-        re_str = k + r"\s*=.*;"
-        target_str = k + " = " + ('true' if v else 'false') + ";"
+        # TODO: make sure 'feature' contains only capitals and underlines
+        re_str = feature + r"\s*=.*;"
+        target_str = feature + " = true;"
         content, count = re.subn(re_str, target_str, content)
         assert(count == 1)
     open(configuration_java, 'w').write(content)
@@ -41,23 +43,28 @@ def compile(path):
     # os.chmod(gradlew, stat.S_IEXEC)
     os.system("chmod 777 " + gradlew)  # TODO: Why os.chmod and chmod +x don't work?
     ret = os.system("cd " + path + " && " + gradlew + " assembleDebug")  # TODO: Change to release
-    os.system("cp " + path + "/Phimpme/build/apk/*.apk ./")
-    return ret, path + "/Phimpme/build/apk/Phimpme-debug-unaligned.apk"
+    #os.system("cp " + path + "/Phimpme/build/outputs/apk/*.apk ./")
+    return ret, path + "/Phimpme/build/outputs/apk/Phimpme-debug-unaligned.apk"
 
 def copy_project(src_path):
     dest_path = "/tmp/Phimpme"
     os.system("rm -r " + dest_path)
+    #import subprocess
+    #print subprocess.check_output("cp -r " + src_path + " " + dest_path)
     r = os.system("cp -r " + src_path + " " + dest_path)
     assert(r == 0)
     return dest_path
 
-def generate(app_name, app_logo, configs):
-    template_path = "/Users/yzq/Documents/GitHub/The-Generator/Android/Phimpme"
+def generate(app_name, app_logo, enables):
+    import sys
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    template_path = "/Users/yzq/Documents/GitHub/phimpme-android/Phimpme"
     path = copy_project(template_path)
     set_name(path, app_name)
     set_logo(path, app_logo)
-    set_config(path, configs)
+    set_enable(path, enables)
     return compile(path)
 
 if __name__ == "__main__":
-    generate(app_name="YZQ-Phimpme", app_logo=None, configs={'ENABLE_MAP': False, 'ENABLE_PHOTO_MANIPULATION': False})
+    generate(app_name="YZQ-Phimpme", app_logo=None, enables=['ENABLE_MAP', 'ENABLE_PHOTO_CAPTURING', 'ENABLE_PHOTO_MANIPULATION'])
--- Phimpme/generator/views.py
+++ Phimpme/generator/views.py
@@ -1,23 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import Http404#HttpResponseServerError
+from django.http import StreamingHttpResponse, Http404
+from django.core.servers.basehttp import FileWrapper
 
 # Create your views here.
 def app_config(request):
     if request.method == 'POST':
-        app_name = request.POST['app_name'].upper()
+        app_name = request.POST['app_name']
         # Read configurations from POST
-        configs = {}
+        enables = []
         for (k, v) in request.POST.items():
             if k.find("enable_") != -1:
-                configs[k] = v
-        
+                enables.append(k.upper())
         # run script
         from generator.gen_script import generate
-        ret, path = generate(app_name = app_name, app_logo = None, configs = configs)
-        print path
+        ret, path = generate(app_name = app_name, app_logo = None, enables = enables)
+        # ret = 0 if successfully generated
         if ret == 0:
-            return render(request, 'app_config.html', {'generated': True, 'download_link':'URL...'})
+            apk_file = file(path)
+            response = StreamingHttpResponse(FileWrapper(apk_file), content_type='application/apk')
+            response['Content-Disposition'] = 'attachment; filename=phimpme-gen.apk'
+            return response
         else:
-            raise Http404#HttpResponseServerError
+            raise Http404
     else:
         return render(request, 'app_config.html')
